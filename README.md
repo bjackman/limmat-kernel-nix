fb1255f6ba5cd22d8d0506c74504651fc30ee3d0
Brendan Jackman <bhenryj0117@gmail.com>
2025-06-30 22:26:18 +0200

    Switch to a devShell-based approach

--- README.md
+++ README.md
@@ -10,39 +10,12 @@ by acting as a layer on top of Limmat.
 This works by using all the toolchains and stuff from nixpkgs, and defining the
 config to refer directly to those binaries.
 
-At present it tries to avoid being too clever with Nix, aside from that. This
-means that the configuration is much more verbose than you might expect: Nix
-makes it very easy to build red blooded American software with a Makefile. But,
-all the magic in nixpkgs to make that happen is designed for _building
-software_, not for building software that builds software. It's not really
-designed to do stuff like make headers available _at runtime_ which is what's
-needed here. Thus, this config kinda recreates some of that logic with a
-different goal in mind.
-
-After thinking about this for a while, I realised I think this is the wrong
-approach. This whole Nix malarkey is gonna be much more interesting if instead
-of just making Limmat marginally more useful it also solves the kernel
-development environment in a more general way. The natural way to do the more
-general thing is via Nix `devShell`s. So, we really wanna define a nice
-`devShell` for the kernel and then allow the Limmat config to run stuff in that
-shell.
-
-In fact, what if we just ask the user to run Limmat directly from that
-`devShell`? This only has one issue: the environment is then invisible to Limmat
-such that it won't affect the result hash. I think this is annoying enough that
-we do still want to figure out how to not require the user to run `nix develop`
-themselves in order for Limmat to work.
-
-It seems that `nix develop` accepts derivation paths. So I think one reasonable
-way to deal with this would just be to have the script in the Limmat config be
-wrapped in a `nix develop <shell derivation> --command ...`.
-
 ## TODO
 
 - [ ] Get all the tasks from `limmat-kernel` coded up via a Nix config.
 - [ ] Set up tests to ensure the configs all run against a golden kernel source,
       which can then be run via `nix flake check`.
-- [ ] Figure out how to create `devShell`s for the scripts that get run in the
+- [x] Figure out how to create `devShell`s for the scripts that get run in the
       Limmat config.
 - [ ] Try to do some general niceness for configuration, like:
   - [ ] Configure the kernel using nice nix shit.
--- limmat-config.nix
+++ limmat-config.nix
@@ -1,99 +1,48 @@
 {
   lib,
   pkgs,
+  writeShellScript,
   writeShellApplication,
+  mkShell,
+  linuxPackages,
 }:
 {
-  config =
-    # Helper to make a derivation that runs a script in an environment with a
-    # bunch of useful stuf available.
-    let
-      writeBuildScript =
-        { name, text }:
-        writeShellApplication rec {
-          inherit name;
-          inherit text;
-          # TODO: Currently describing the build env completely manually.
-          # This might actually be a desirable way to do it, but maybe it's
-          # way more sensible to try to more directly expose the build env
-          # of the NixOS kernel package. I just haven't figured out how to
-          # do that.
-          runtimeInputs = with pkgs; [
-            # Basic shit that is included by default in the stdenv. I'm not
-            # sure if it's really expected that these are needed but
-            # including them lets me test with `nix run --unset PATH` to
-            # check purity which is nice.
-            bash
-            coreutils
-            gnused
-            gnumake
-            gnugrep
-            diffutils
-            gawk
-            findutils
-
-            # The core kernel build deps.
-            gcc
-            bison
-            flex
-            perl
-            bc
-            openssl
-            elfutils
-            cpio
-            libelf
-
-            # Other stuff.
-            ccache
-          ];
-          # TODO: Even if we did actually prefer to build the environment
-          # quite manually, it seems unlikely that specifying these
-          # variables completely explicitly like this would really be
-          # necessary, but I'm not sure how this happens magically in
-          # nixpkgs when you specify a library as a buildInput. Maybe that
-          # magic can be reused here or maybe not. I think that magic is to
-          # do with the obtusely-documented "Package setup hooks". Those are
-          # specified as being unstable between Nixpkgs releases so maybe
-          # doing it manually like this really is preferred...
-          # https://nixos.org/manual/nixpkgs/stable/#ssec-setup-hooks
-          runtimeEnv = {
-            # Set HOSTCFLAGS to point to all the necessary headers to build
-            # host binaries.
-            #
-            # Headers are generally in the .dev output of the package,
-            # but no all packages have this output. catAttrs is a convenient
-            # way to get that attribute for the ones that have it and ignore
-            # the ones that don't.
-            HOSTCFLAGS = lib.concatStringsSep " " (
-              map (pkgDev: "-isystem ${pkgDev}/include") (builtins.catAttrs "dev" runtimeInputs)
-            );
-            # Define HOSTLDFLAGS so we can link against libraries when
-            # building host stuff. This is a bit simpler because we can
-            # assume that all the packages have a .out output.
-            HOSTLDFLAGS = lib.concatStringsSep " " (map (pkg: "-L ${pkg.out}/lib") runtimeInputs);
-            # This may or may not make ccache work better I dunno.
-            CCACHE_SLOPPINESS = "time_macros";
-          };
-        };
-    in
-    {
-      tests = [
-        {
-          name = "build_min";
-          command =
-            let
-              pkg = writeBuildScript {
-                name = "test-build_min";
-                text = ''
-                  make -j tinyconfig
-                  scripts/config -e 64BIT -e -WERROR -e OBJTOOL_WERROR
-                  make -j olddefconfig
-                  make -sj"$(nproc)" vmlinux CC='ccache gcc' KBUILD_BUILD_TIMESTAMP= 2>&1
-                '';
-              };
-            in
-            "${pkg}/bin/test-build_min";
-        }
-      ];
-    };
+  config = {
+    tests = [
+      {
+        name = "build_min";
+        command =
+          let
+            script = writeShellScript "build_min" ''
+              make -j tinyconfig
+              scripts/config -e 64BIT -e -WERROR -e OBJTOOL_WERROR
+              make -j olddefconfig
+              make -sj"$(nproc)" vmlinux CC='ccache gcc' KBUILD_BUILD_TIMESTAMP= 2>&1
+            '';
+            kernelDevShell = mkShell {
+              inputsFrom = [ linuxPackages.kernel ];
+              packages = [ pkgs.ccache ];
+            };
+          in
+          # Alright, so we are gonna want to define a devShell anyway. And
+          # devShells include all the necessary magic for setting up the build
+          # environment, which you don't usually get at runtime: for example, if
+          # you set up openssl as a runtime dependency of the script, the
+          # library will be there but it won't be set up in your LDFLAGS or
+          # whatever. So it really makes sense to want to run the script in a
+          # devShell (after all, the whole idea of Limmat is that it encodes
+          # "tests" that are similar to what you run in your interactive shell).
+          # How do you run something inside a devShell (at runtime)? I'm not
+          # sure. As best I can tell, the thing that mkShell produces is not
+          # really considered API - there is text at the top of it saying so.
+          # It's considered an internal implementation detail of 'nix-shell' and
+          # 'nix develop'. So.... let's just call `nix develop`...? I'm pretty
+          # sure there are gonna be sketchy consequences of this but I'm not
+          # really sure what they will be.
+          writeShellScript "nix-develop_build_min" ''
+            ${pkgs.nix}/bin/nix develop ${kernelDevShell.drvPath} --command ${script}
+          '';
+      }
+    ];
+  };
 }
