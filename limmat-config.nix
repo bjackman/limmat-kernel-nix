7461792daebf13892420a0df6275a0979a113ec3
Brendan Jackman <bhenryj0117@gmail.com>
2025-06-30 22:51:36 +0200

    Add comment about mysterious Nix shit

--- limmat-config.nix
+++ limmat-config.nix
@@ -7,6 +7,14 @@
   linuxPackages,
 }:
 {
+  # Why does this need to return everything inside a ".config" field instead of
+  # just directly returning the config? Well, let me explain. Basically, it's
+  # quite simple. Quite simply, I do not know. I don't know why this is needed.
+  # I think it's some sort of outcome of the Nix black magic that goes on with
+  # callPackage. If you try to serialise the direct result of this package into
+  # JSON it will fail because it's actually some Nix black magic and not just a
+  # normal attribute set. But if you instead access an attribute of the returned
+  # value then you get a nice red blooeded American attrset.
   config = {
     tests = [
       {
