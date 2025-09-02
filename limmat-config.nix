696caf353ac12fa33d230903d888be3263916161
Brendan Jackman <jackmanb@google.com>
2025-09-02 09:36:06 +0000

    Drop devShell stuff from config
    I think this is too complicated. Let's just weaken the requirements:
    
    - Require the user to run limmat from a devShell
    - Give up on having nice hermetic limmat config with the hashes in it.

--- limmat-config.nix
+++ limmat-config.nix
@@ -6,31 +6,6 @@
   linuxPackages,
   kernelDevShell,
 }:
-let
-  # Alright, so we are gonna want to define a devShell anyway. And
-  # devShells include all the necessary magic for setting up the build
-  # environment, which you don't usually get at runtime: for example, if
-  # you set up openssl as a runtime dependency of the script, the
-  # library will be there but it won't be set up in your LDFLAGS or
-  # whatever. So it really makes sense to want to run the script in a
-  # devShell (after all, the whole idea of Limmat is that it encodes
-  # "tests" that are similar to what you run in your interactive shell).
-  # How do you run something inside a devShell (at runtime)? I'm not
-  # sure. As best I can tell, the thing that mkShell produces is not
-  # really considered API - there is text at the top of it saying so.
-  # It's considered an internal implementation detail of 'nix-shell' and
-  # 'nix develop'. So.... let's just call `nix develop`...? I'm pretty
-  # sure there are gonna be sketchy consequences of this but I'm not
-  # really sure what they will be.
-  # OKAY: one of the sketchy outcomes of this is that we cannot run this
-  # inside the Nix build sandbox. This prevents us from defining a flake
-  # check that verifies the configuration. Hmm.
-  mkDevShellScript =
-    name: text:
-    writeShellScript "nix-develop_${name}" ''
-      ${pkgs.nix}/bin/nix develop ${kernelDevShell.drvPath} --command ${writeShellScript name text}
-    '';
-in
 {
   # Why does this need to return everything inside a ".config" field instead of
   # just directly returning the config? Well, let me explain. Basically, it's
@@ -44,7 +19,7 @@ in
     tests = [
       {
         name = "build_min";
-        command = mkDevShellScript "build_min" ''
+        command = ''
           set -eux
 
           make -j tinyconfig
