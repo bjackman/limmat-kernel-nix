ad8bf0651574f6be391d081eb95ccb27011d0d01
Brendan Jackman <jackmanb@google.com>
2025-09-03 15:10:15 +0000

    nix fmt

--- flake.nix
+++ flake.nix
@@ -27,9 +27,10 @@
         # This package will be used for golden testing, and also to grab the
         # toolchains etc for the devShell.
         refKernel = pkgs.linuxPackages.kernel;
-        limmatConfig = (pkgs.callPackage ./limmat-config.nix {
-          vm-run = self.packages."${system}".vm-run;
-        }).config;
+        limmatConfig =
+          (pkgs.callPackage ./limmat-config.nix {
+            vm-run = self.packages."${system}".vm-run;
+          }).config;
         format = pkgs.formats.toml { };
       in
       {
