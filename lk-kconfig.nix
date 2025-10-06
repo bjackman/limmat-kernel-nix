4473f86bae5a315a002415be1515a9f2ce64ca99
Brendan Jackman <jackmanb@google.com>
2025-10-06 15:28:02 +0000

    lk-kconfig: Use string interpolation to prevent rebuilds

--- lk-kconfig.nix
+++ lk-kconfig.nix
@@ -3,7 +3,14 @@
   pkgs,
 }:
 let
-  fragments = ./kconfigs;
+  # Interpolating ./kconfigs into a string is important here, it causes the
+  # content to be build into a separate derivation with its own hash. Otherwise,
+  # it just refers to the kconfigs subdirectory of the source derivation for the
+  # overall flake. That turns out to be really undesirable because that
+  # derivation path changes if anything in the flake changes, which means that
+  # this derivation ends up being different even if none of the files it refers
+  # to changed.
+  fragments = "${./kconfigs}";
 in
 pkgs.writeShellApplication {
   name = "lk-kconfig";
