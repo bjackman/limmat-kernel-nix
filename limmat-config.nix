ce336edbb4a617ebe50966666bba4f928f259f86
Brendan Jackman <jackmanb@google.com>
2026-02-09 10:52:56 +0000

    limmat-config: Disable vsock
    The vsock CID is a global resource - that doesnt' lead to race
    conditions in Limmat due to the `qemu_throttle`, but it does mean while
    we're running the tests the resource is taken.
    
    But also, GHA doesn't allow it:
    
    https://github.com/bjackman/limmat-kernel-nix/actions/runs/21754857035/job/62762183009

--- limmat-config.nix
+++ limmat-config.nix
@@ -170,7 +170,7 @@ let
       # 0x20 means is TAINT_BAD_PAGE.
       timeout --signal=KILL 600s \
         ${lk-vm}/bin/lk-vm --kernel "$kernel" --ktests="$KTEST_ARGS" \
-        --ktests-output="$LIMMAT_ARTIFACTS" \
+        --vsock-cid=-1 --ktests-output="$LIMMAT_ARTIFACTS" \
         --cmdline "$KERNEL_ARGS panic_on_warn=1 panic_on_taint=0x20"
     '';
   };
