fb260fd4f0ce198c255e4e2be20bc8679386ed61
Brendan Jackman <jackmanb@google.com>
2026-04-01 15:26:34 +0200

    lk-vm: Break out NixOS module for running ktests

--- lk-vm/modules/base.nix
+++ lk-vm/modules/base.nix
@@ -5,23 +5,14 @@
   self,
   ...
 }:
-let
-  ktestsOutputDir = "/mnt/ktests-output";
-  # I/O port that will be used for the isa-debug-exit device. I don't know
-  # how arbitrary this value is, I got it from Gemini who I suspect is
-  # cargo-culting from https://os.phil-opp.com/testing/
-  qemuExitPortHex = "0xf4";
-in
 {
+  imports = [ ./ktests.nix ];
+
   nixpkgs.overlays = [ self.overlays.guest ];
   networking.hostName = "testvm";
   virtualisation.vmVariant = {
     virtualisation = {
       graphics = false;
-      qemu.options = [
-        "-device"
-        "isa-debug-exit,iobase=${qemuExitPortHex},iosize=0x04"
-      ];
       # Tell the VM runner script that it should mount a directory on the
       # host, named in the environment variable, to /mnt/kernel. That
       # variable must point to a directory. This is coupled with the script
@@ -31,22 +22,15 @@ in
           source = "$KERNEL_TREE";
           target = "/mnt/kernel";
         };
-        ktests-output = {
-          source = "$KTESTS_OUTPUT_HOST";
-          target = ktestsOutputDir;
-        };
       };
       # Attempt to ensure there's space left over in the rootfs (which
       # may be where /tmp is).
       diskSize = 2 * 1024; # Megabytes
+      memorySize = 16 * 1024;
       # This seems to speed up boot a bit, and also I'm finding some KVM
       # selftests hang the VM on a uniprocessor system.
       cores = 8;
     };
-
-    # mm selftests are hard-coded to put stuff in /tmp which has very
-    # little space on a NixOS VM, unless it's a tmpfs.
-    boot.tmp.useTmpfs = true;
   };
   system.stateVersion = "25.05";
   services.getty.autologinUser = "root";
@@ -62,33 +46,6 @@ in
   # those in. We need mkForce because qemu-guest.nix doesn't respect
   # boot.inirtd.includeDefaultModules.
   boot.initrd.kernelModules = lib.mkForce [ ];
-
-  # As an easy way to be able to run it from the kernel cmdline, just
-  # encode ktests into a systemd service. You can then run it with
-  # systemd.unit=ktests.service.
-  systemd.services.ktests = {
-    script = ''
-      # Convert the KTESTS_ARGS to an array so it can be expanded
-      # without glob expansion.
-      IFS=' ' read -r -a args <<< "$KTESTS_ARGS"
-      # Writing the value v to the isa-debug-exit port will cause QEMU to
-      # immediately exit with the exit code `v << 1 | 1`.
-      ${pkgs.ktests}/bin/ktests \
-        --junit-xml ${ktestsOutputDir}/junit.xml --log-dir ${ktestsOutputDir} \
-        "''${args[@]}" \
-        || ${pkgs.ioport}/bin/outb ${qemuExitPortHex} $(( $? - 1 ))
-    '';
-    serviceConfig = {
-      Type = "oneshot";
-      StandardOutput = "tty";
-      StandardError = "tty";
-    };
-    onSuccess = [ "poweroff.target" ];
-  };
-
-  # Some mmtests fail if the system doesn't have swap. I don't wanna
-  # configure proper swap but let's try zswap.
-  # zramSwap.enable = true;
 
   # Disable all networking stuff. The goal here was to speed up boot, it
   # doesn't seem to have a measurable effect but at least it avoids
---
+++ lk-vm/modules/ktests.nix
@@ -0,0 +1,57 @@
+# This module defines the stuff for running ktests automatically via systemd,
+# it is coupled to the --ktests arg of lk-vm.sh.
+{ pkgs, ... }:
+let
+  ktestsOutputDir = "/mnt/ktests-output";
+  # I/O port that will be used for the isa-debug-exit device. I don't know
+  # how arbitrary this value is, I got it from Gemini who I suspect is
+  # cargo-culting from https://os.phil-opp.com/testing/
+  qemuExitPortHex = "0xf4";
+in
+{
+  virtualisation.vmVariant = {
+    virtualisation = {
+      qemu.options = [
+        "-device"
+        "isa-debug-exit,iobase=${qemuExitPortHex},iosize=0x04"
+      ];
+      sharedDirectories = {
+        ktests-output = {
+          source = "$KTESTS_OUTPUT_HOST";
+          target = ktestsOutputDir;
+        };
+      };
+    };
+
+    # mm selftests are hard-coded to put stuff in /tmp which has very
+    # little space on a NixOS VM, unless it's a tmpfs.
+    boot.tmp.useTmpfs = true;
+  };
+
+  # As an easy way to be able to run it from the kernel cmdline, just
+  # encode ktests into a systemd service. You can then run it with
+  # systemd.unit=ktests.service.
+  systemd.services.ktests = {
+    script = ''
+      # Convert the KTESTS_ARGS to an array so it can be expanded
+      # without glob expansion.
+      IFS=' ' read -r -a args <<< "$KTESTS_ARGS"
+      # Writing the value v to the isa-debug-exit port will cause QEMU to
+      # immediately exit with the exit code `v << 1 | 1`.
+      ${pkgs.ktests}/bin/ktests \
+        --junit-xml ${ktestsOutputDir}/junit.xml --log-dir ${ktestsOutputDir} \
+        "''${args[@]}" \
+        || ${pkgs.ioport}/bin/outb ${qemuExitPortHex} $(( $? - 1 ))
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      StandardOutput = "tty";
+      StandardError = "tty";
+    };
+    onSuccess = [ "poweroff.target" ];
+  };
+
+  # Some mmtests fail if the system doesn't have swap. I don't wanna
+  # configure proper swap but let's try zswap.
+  # zramSwap.enable = true;
+}
